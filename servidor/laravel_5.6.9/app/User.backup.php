f87e5973ac32f585e704a72bf8938c92fb69f722
Luis Alberto Alvarez Jiménez <wiz_Alberto@hotmail.com>
2018-03-16 11:53:26 -0600

    Arreglo para base de datos
    Se agrego el sistema de seeds para la tabla de usuarios
    se modifico el archivo .env en laravel para concetar a la base dedatos

--- servidor/laravel_5.6.9/app/User.backup.php
+++ servidor/laravel_5.6.9/app/User.backup.php
@@ -1,29 +1,29 @@
 <?php
 
-namespace App;
+/*namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable;*/
 
-    /**
+    /* *
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'nombres','apellidos', 'email', 'password', 'telefono','sexo'
-    ];
+   /* protected $fillable = [
+        'name', 'email', 'password',
+    ];*/
 
-    /**
+    /* *
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
+    /*protected $hidden = [
         'password', 'remember_token',
     ];
-}
+}*/
--- servidor/laravel_5.6.9/database/migrations/create_users_table.backup.php
+++ servidor/laravel_5.6.9/database/migrations/create_users_table.backup.php
@@ -1,38 +1,35 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
+/*use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
-{
-    /**
+{*/
+    /* *
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    /*public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombres');
-            $table->string('apellidos');
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('telefono')->nullable();
-            $table->string('sexo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
-    }
+    }*/
 
-    /**
+    /* *
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
+    /*public function down()
     {
         Schema::dropIfExists('users');
     }
-}
+}*/
