e640107b147f85401ca162dfaf9a77cdec5dc344
Luis Alberto Alvarez Jiménez <wiz_Alberto@hotmail.com>
2018-03-26 12:36:09 -0600

    cambios menores

--- web/angular524/src/app/services/user.service.ts
+++ web/angular524/src/app/services/user.service.ts
@@ -3,28 +3,33 @@ import { User } from '../models/user';
 import {Http, Response} from '@angular/http'
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Rx';
+import {HttpClient} from '@angular/common/http';
 
 
 @Injectable()
 export class UserService {
 
-  constructor(private http:Http) { }
-  baseUrl='http://template3.test/laravel_5.6.9/public/api/users';
-  baseUrl2='http://template3.test/laravel_5.6.9/public/api/login';
+  constructor(private http:Http, private httpClient:HttpClient) { }
+  baseUrl='http://template3.test/laravel_5.6.9/public/api/';
 
   getUsers(): Observable<User[]>{
-  	return this.http.get(this.baseUrl).map((response: Response) => response.json());
+  	return this.http.get(this.baseUrl+'users').map((response: Response) => response.json());
   }
 
   createUser(user:Object): Observable<User[]>{
-  	return this.http.post(this.baseUrl,user).map((response: Response) => response.json()).catch((error:any) => Observable.throw(error.json().error || {message:"Error del servidor"}));
+  	return this.http.post(this.baseUrl+'users',user).map((response: Response) => response.json()).catch((error:any) => Observable.throw(error.json().error || {message:"Error del servidor"}));
+  }
+
+  uploadImage(image:any){
+    /*console.log("servicio uploadImage");*/
+    return this.httpClient.post(this.baseUrl+'upload',image);
   }
 
   getUser(id:String): Observable<User[]>{
-     return this.http.get(this.baseUrl + '/'+id).map((response: Response) => response.json());
+     return this.http.get(this.baseUrl + 'users/'+id).map((response: Response) => response.json());
    }
-   
-   updateUser(user:Object): Observable<User[]>{
+
+  updateUser(user:Object): Observable<User[]>{
      const apiUrl = 'http://template3.test/laravel_5.6.9/public/api/users';
      const url = `${apiUrl}/${user["id"]}`;
      console.log(url);
@@ -32,13 +37,13 @@ export class UserService {
    }
 
    logUser(user:Object): Observable<User[]>{
-    return this.http.post(this.baseUrl2,user).map((response: Response) => response.json()).catch((error:any) => Observable.throw(error.json().error || {message:"Error del servidor"}));
+  	return this.http.post(this.baseUrl+'login',user).map((response: Response) => response.json()).catch((error:any) => Observable.throw(error.json().error || {message:"Error del servidor"}));
   }
 
-  deleteUser(id:String){
+  deleteUser(id:String): Observable<User[]>{
       /*console.log('Prueba de llamado de función desde el service');
       console.log(this.baseUrl+'/'+id);*/
-      return this.http.delete(this.baseUrl + '/'+ id); /*.catch((error:any) => Observable.throw(error.json().error || {message:"Error del servidor"}));*/
+      return this.http.delete(this.baseUrl + 'users/'+ id).map((response: Response) => response.json());
     }
 
 }
