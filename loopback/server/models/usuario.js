14301828df0035bb72fc38a29681d645a97a4d7c
Ivan Pacheco <iapachecorios@gmail.com>
2018-11-06 13:06:06 -0600

    Endpoint cambio de imagen de perfil TEMP-4

--- loopback/server/models/usuario.js
+++ loopback/server/models/usuario.js
@@ -164,6 +164,39 @@ module.exports = function(Usuario) {
     };
 
 
+    /**
+     * changes the user's prifile image for a new one
+     * @param {object} newImage new image in base 64
+     * @param {Function(Error, object)} callback
+     */
+
+    Usuario.prototype.changeProfileImage = function(newImage, callback) {
+        var Upload = app.models.Upload
+        var actual = this;
+        // TODO
+        var newProfileImage = {
+            encodedFileContainer: "profileImages",
+            base64File: newImage.profileImage.base64ProfileImage,
+            fileExtention: newImage.profileImage.base64ProfileImageExtention
+        }
+        Upload.replaceFileBase64File(actual.profileImageId, newProfileImage, function(err, res){
+            if (err) return callback(err);
+
+            actual.profileImage(res);
+            Usuario.upsert(actual, function(err, updatedUser){
+                if (err) return callback(err);
+                  Usuario.findOne({
+                    where:{
+                        id: updatedUser.id
+                    }
+                  }, function(err, userWR){
+                    if (err) return callback(err);
+                    callback(null, userWR);
+                  });
+              })
+        })
+    };
+
 
     /**
      * updates a user's push token
@@ -217,4 +250,22 @@ module.exports = function(Usuario) {
             }
         })
     }; 
+
+    /**
+     * Deletes push tokens before logout
+     * @param {object} body token to be saved
+     * @param {Function(Error)} callback
+     */
+    Usuario.beforeRemote('logout', function(context, callback) {
+        Usuario.find({where: {id : context.req.accessToken.userId}}, function(err, usuario) {
+            if(err) return callback(err)
+
+            context.req.body.tokens.forEach(token => {
+                let body = { token : token }
+                console.log("BODY", body)
+
+                usuario.deletePushToken(body, null)
+            });
+        })
+    });
 };
