9543be65abea8b181284ae37296b7be27e7145f3
Pablo Cardona <a16300051@live.ceti.mx>
2019-10-24 12:08:05 -0500

    Signup view

--- mobile/src/app/pages/signup/signup.page.ts
+++ mobile/src/app/pages/signup/signup.page.ts
@@ -7,6 +7,7 @@ class Account {
   username: string;
   email: string;
   password: string;
+  repeatPassword: string;
 }
 
 @Component({
@@ -16,13 +17,15 @@ class Account {
 })
 export class SignupPage extends ComponentBase implements OnInit {
   signupAccount: Account = new Account(); 
-  constructor() { }
 
   ngOnInit() {
     this.disableMenu();
   }
 
   public async OnSignup() {
+    if(! await this.isUserDataValid(this.signupAccount)){
+      return;
+    }
     this.ShowLoading()
     this.api.post("/Usuarios/register", this.signupAccount, false).subscribe(
       userToken => this.GetUserWithAPIToken(userToken),
@@ -30,34 +33,48 @@ export class SignupPage extends ComponentBase implements OnInit {
     )
   }
 
-
+  public async isUserDataValid(signupAccount : Account){
+    if(this.signupAccount.password != this.signupAccount.repeatPassword){
+      console.log("Too bad");
+      this.ShowToast("Las contraseñas no coinciden");
+      return false;
+    }
+    return true;
+  }
 
   public async GetUserWithAPIToken(token) {
-    this.ShowLoading()
-    await this.storage.clear()
-    await this.api.setToken(token.id)
+    this.ShowLoading();
+    await this.storage.clear();
+    await this.api.setToken(token.id);
 
     this.api.get("/Usuarios/withCredentials", true).subscribe(
       userFromServer => this.SaveUserData(userFromServer),
-      error => this.HandleAPIError(error)     
+      error => this.HandleAPIError(error)
     )
   }
 
   private async SaveUserData(userFromServer: JSON) {
-    await this.storage.set("user", userFromServer)
-    await this.storage.set("ttl", moment().add(1209600, 's').toISOString())
-    await this.AfterSuccessfulSignup()
+    await this.storage.set("user", userFromServer);
+    await this.storage.set("ttl", moment().add(1209600, 's').toISOString());
+    await this.AfterSuccessfulSignup();
   }  
 
   private async AfterSuccessfulSignup() {
-    this.DismissLoading()
-    this.enableMenu()
-    this.events.publish('user:logged', true)
-    this.navController.navigateRoot('dashboard')
+    this.DismissLoading();
+    this.enableMenu();
+    this.events.publish('user:logged', true);
+    this.navController.navigateRoot('dashboard');
   }
 
   public goToUrl(url) {
     this.navController.navigateRoot(url);
   }
 
+  public async OnSocialLoginStart() {
+    this.ShowLoading();
+  }
+
+  public async OnSocialLoginError(data) {
+    this.ShowToast(data);
+  }
 }
