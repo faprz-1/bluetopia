c9a9b63c4742c621ffb94a1ebd530baff5bc79e6
Juan Carlos <juanc.sanchezgtz@gmail.com>
2020-03-11 17:49:15 -0600

    deploy script

--- deploy-script-develop.sh
+++ deploy-script-develop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd repos/template4.0
+echo "DEPLOYYYYYYYY"
 
-docker-compose -f docker-compose-dev.yml down --rmi all
-cker-compose -f docker-compose-dev.yml up -d --build
+#docker-compose -f docker-compose-dev.yml down --rmi all
+#cker-compose -f docker-compose-dev.yml up -d --build
