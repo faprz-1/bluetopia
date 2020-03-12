d009bc621cf7f90dcf0a79dbe301c58a3eb441c5
Juan Carlos <juanc.sanchezgtz@gmail.com>
2020-03-12 12:49:58 -0600

    better deploy script

--- deploy-script-develop.sh
+++ deploy-script-develop.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+export PROYECT=template4.0
 
-cd ~/repos/template4.0
+echo "Switching dir to proyect: $PROYECT"
+cd ~/repos/$PROYECT
+
+echo "Setting Rep"
 #git reset --hard
 git checkout master
 git pull
 
+echo "Removing old docker containers"
 sudo docker-compose -f docker-compose-dev.yml down --rmi all
+
+echo "Rebuilding docker containers"
 sudo docker-compose -f docker-compose-dev.yml up -d --build
