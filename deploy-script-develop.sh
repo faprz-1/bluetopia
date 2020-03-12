4b2c6c1fc842bde4e46ab1dd3716caf3960125de
Juan Carlos <juanc.sanchezgtz@gmail.com>
2020-03-12 10:29:38 -0600

    sudo

--- deploy-script-develop.sh
+++ deploy-script-develop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd ~/repos/template4.0
-git reset --hard
+#git reset --hard
 git checkout master
 git pull
 
-docker-compose -f docker-compose-dev.yml down --rmi all
-cker-compose -f docker-compose-dev.yml up -d --build
+sudo docker-compose -f docker-compose-dev.yml down --rmi all
+sudo docker-compose -f docker-compose-dev.yml up -d --build
