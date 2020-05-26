6f3d0da3b798c343a6e66968d4de4715d778b510
Antonio Romano <antonioromano@jarabesoft.com>
2020-05-26 10:11:04 -0500

    add function to validate a domain

--- loopback/server/models/admin-mail.js
+++ loopback/server/models/admin-mail.js
@@ -1,5 +1,14 @@
 'use strict';
+var dns = require("dns");
+var sys = require('sys');
 
 module.exports = function (Adminmail) {
+
+    Adminmail.CheckDomainAvailability = function(domain, callback){
+        dns.resolve4(domain, (err, validated) => {
+            if(err) return callback(null, false);
+            return callback(null, true);
+        })
+    }
 
 };
