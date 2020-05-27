33817645ff8ea4223a08229c568235f6e695d329
adithlbp <adith-lbp@hotmail.com>
2020-05-27 09:49:59 -0500

    mobile notifications endponts

--- loopback/server/models/push-tokens.js
+++ loopback/server/models/push-tokens.js
@@ -1,103 +1,47 @@
 'use strict';
 
-var disableRelationMethods = require("../helpers/model-methods-helper").disableRelationMethods;
 const request = require('request');
+const oneSignalURL = "https://onesignal.com/api/v1/notifications";
+const app_id = "15d45c11-5e6d-4875-91e9-6d2939236c35";
+const mobileKey = "Basic ZjQwODc0NTktMzA0Ny00YmI5LTk0M2ItYTQwYWVlZDFhNDQ2";
 
-module.exports = function(PushTokens) {
-    disableRelationMethods(PushTokens)
-    
-    PushTokens.sendPushNotification = function(pushTokens,
-        not = {
-            tag: "jarabeDefault",
-            title: "default",
-            content: "body default",
-            icon: "",
-            image:"",
-            link: "",
-            vibrate: true,
-            onclick: () => {console.log("default click")}
-        }, callback) {
-        var desktopKey = "key=AAAACBx3Rhk:APA91bHbX3QnY9HWv8vtnIe2sGU_cU3uxUpV2C7pUhzny4cjuSZNBzRY00pORUBpc4p_AwDptAB3tagOWibz0o1SqYWVYXI51gtxFbp6HqE869m9BAYxqCAUVNQUsv6ezAd-GoNznvtUuT2-dDwad7Pt6gycHhVVkg"
-        var mobileKey = "key=AAAAzXAWB2M:APA91bGOdatSPp8zVZ7xw5Y5v8t3qRr3OpeUM4qtGJm_qbqFhUX7tFJJIic4PkUhC8ogovkZvGts1gS5NtSGe-aWt5ukbhDoGmknXExv1FBzLLuF9sHlfisB97eplOHtdG2LerHToCPZ"
-        if (pushTokens.length != 0) {
+// var desktopKey = "key=AIzaSyB14fCDhNGsNrExghdGRZYZLWrnAHdWI-M"
 
-            const fireBaseURI = "https://fcm.googleapis.com/fcm/send"
+module.exports = function (PushToken) {
 
-            // prepare desktop notifications
-            var desktop_headers = {
-                "Content-Type": "application/json",
-                "Authorization": desktopKey
-            }
-            var notificationOptions =       
-            {
-                priority: "high",
-                content_available: (not.content)?true:false,
-                sound: "default",
-                vibrate: true,
-                body: not.content,
-                title: not.title,
-                click_action: not.link,
-                icon: not.image,
-                tag: not.tag,
-                onclick: not.onclick
-            }
-            var desktop_data = {
-                notification: notificationOptions,
-                registration_ids: []
-            }
-
-            // prepare mobile notifications
-            var mobile_headers = {
-                "Content-Type": "application/json",
-                "Authorization": mobileKey
-            }
-            var mobile_data = {
-                notification: notificationOptions,
-                registration_ids: []
-            }
-
-            pushTokens.forEach(pT => {
-                console.log("Pushtocken", pT)
-                if (pT.mobile == true)
-                    mobile_data.registration_ids.push(pT.id)
-                else
-                    desktop_data.registration_ids.push(pT.id)
-            });
-            console.log("count mobile",mobile_data.registration_ids.length)
-            console.log("count desktop",desktop_data.registration_ids.length)
-            var result = {
-                desktop: {},
-                mobile: {}
-            }
-            // send desktop notifications
-            request.post(fireBaseURI, {
-                body: JSON.stringify(desktop_data),
-                headers: desktop_headers
-            }, function (err, response, body) {
-                if (err) return callback(err);
-
-                if (body) {
-                    result.desktop = body;
-
-                    // send mobile notificationsk
-                    request.post(fireBaseURI, {
-                        body: JSON.stringify(mobile_data),
-                        headers: mobile_headers
-                    }, function (err, response, body) {
-                        if (err) return callback(err);
-
-                        if (body) {
-                            result.mobile = body;
-
-                            return callback(null, result);
-                        } else {
-                            return callback(null, response)
-                        }
-                    })
-                } else {
-                    return callback(null, response)
-                }
-            })
+    PushToken.sendMobilePushNotification = function (pushTokens, notification, callback) {
+        if (!pushTokens || pushTokens.length <= 0) {
+            return callback(null, 'No tokens');
         }
+
+        let headers = {
+            "Content-Type": "application/json",
+            "Authorization": mobileKey
+        }
+
+        let notificationBody = {
+            app_id : app_id,
+            data : notification.data,
+            contents : { en :  notification.content, es :  notification.content },
+            headings : { en : notification.heading, es : notification.heading },
+            include_player_ids : []
+            // "image": "https://scontent.fgdl4-1.fna.fbcdn.net/v/t1.0-9/50728512_2110076052393262_3123088630382329856_o.png?_nc_cat=103&_nc_ht=scontent.fgdl4-1.fna&oh=045b806a600cd3dc959b54ae1e739d73&oe=5D2B7633"
+        }
+        pushTokens.forEach(token => {
+
+            if (token.mobile == true){
+                notificationBody.include_player_ids.push(token.id)
+            }
+        });
+        
+        request.post(oneSignalURL, {
+            body: JSON.stringify(notificationBody),
+            headers: headers
+        }, function (err, response, body) {
+            if (err) return callback(err);
+            return callback(null, true);
+        })
+    
     }
+
 };
--- loopback/server/models/usuario.js
+++ loopback/server/models/usuario.js
@@ -364,91 +364,56 @@ module.exports = function(Usuario) {
     };
 
 
-    /**
-     * updates a user's push token
-     * @param {object} body token to be saved
-     * @param {Function(Error)} callback
-     */
-
-    Usuario.prototype.updatePushToken = function(body, callback) {
-        var PushTokens = Usuario.app.models.PushTokens;
-        var actual = this;
-        // TODO
-        PushTokens.findById(body.token.id, function(err, res) {
-            if (err) return callback(err);
-
-            if (!res) {
-                var newToken = {
-                    id: body.token.id,
-                    usuarioId: actual.id,
-                    mobile: true
-                }
-                PushTokens.create(newToken, function(err, res) {
-                    if (err) return callback(err);
-
-                    callback(null, true);
-                })
-            } else {
-                callback(null, false)
-            }
-        })
-    };
-
     /** 
      * updates a user's push token 
-     * @param {object} body token to be saved 
+     * @param {object} data token to be saved 
      * @param {Function(Error)} callback 
      */
 
-    Usuario.prototype.updatePushTokenMobile = function(body, callback) {
-        var PushTokens = Usuario.app.models.PushTokens;
-        var actual = this;
+    Usuario.prototype.updatePushToken = function(data, callback) {
+        let PushToken = Usuario.app.models.PushToken;
+        let actual = this;
         // TODO 
-        PushTokens.findById(body.token, function(err, res) {
+        PushToken.findById(data.token, function(err, existing) {
             if (err) return callback(err);
-
-            if (!res) {
-                var newToken = {
-                    id: body.token,
-                    usuarioId: actual.id,
-                    mobile: true
-                }
-                PushTokens.create(newToken, function(err, res) {
-                    if (err) return callback(err);
-
-                    callback(null, "Registro exitoso");
-                })
-            } else {
-                PushTokens.upsert({ id: res.id, mobile: true }, (err, updated) => {
-                    if (err) return callback(err)
-
-                    return callback(null, "token ya registrado");
-                })
+            if(existing){ return callback(null, true) }
+            let newToken = {
+                id: data.token,
+                usuarioId: actual.id,
+                mobile: data.isMobile
             }
+            PushToken.create(newToken, function(err, res) {
+                if (err) return callback(err);
+
+                return callback(null, true);
+            })
+            
         })
     };
+
     /**
      * updates a user's push token
-     * @param {object} body token to be saved
+     * @param {object} data token to be saved
      * @param {Function(Error)} callback
      */
 
-    Usuario.prototype.deletePushToken = function(body, callback) {
-        var PushTokens = Usuario.app.models.PushTokens;
-        var actual = this;
-        // TODO
-        actual.PushTokens.findById(body.token, function(err, res) {
+    Usuario.deletePushToken = function(token, callback) {
+        let PushToken = Usuario.app.models.PushToken;
+       
+        PushToken.findById(token, function(err, res) {
             if (err) return callback(err);
-            if (res) {
-                PushTokens.destroyById(body.token, function(err, res) {
-                    if (err) return callback(err);
 
-                    callback(null);
-                })
-            }
+            if (!res) { return callback(null, true ) }
+            PushToken.destroyById(token, function(err, res) {
+                if (err) return callback(err);
+
+                return callback(null, true);
+            })
+            
         })
     };
 
+    
     /**
      * Deletes push tokens before logout
      * @param {object} body token to be saved
