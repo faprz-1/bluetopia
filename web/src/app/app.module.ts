a44f5dc11d5cb3c710492e709878e5a2adcca91c
adithlbp <adith-lbp@hotmail.com>
2020-05-27 10:22:45 -0500

    firebase removed

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,20 +10,12 @@ import { PapaParseModule } from 'ngx-papaparse';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
-// firebase
-import { AngularFireModule } from 'angularfire2';
-import * as firebase from 'firebase';
-import { firebaseConfig } from './../environments/firebase.config';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-firebase.initializeApp(firebaseConfig);
-
 ///services
 import { SharedService } from "./shared/services/shared.service";
 import { ApiService } from "./services/api.service";
 import { HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './services/auth.guard';
 import { MessagingService } from './services/messaging.service';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { FilterPipe } from './filter.pipe';
 import { ToastService } from './services/toast.service';
 import { ToastrModule } from 'ngx-toastr';
@@ -37,8 +29,6 @@ import { LoadingService } from './services/loading.service';
     BrowserAnimationsModule,
     FormsModule,
     routing,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
     ToastrModule.forRoot(),
     PapaParseModule
   ],
@@ -56,7 +46,6 @@ providers: [
     AuthGuard,
     AuthSGuard,
     MessagingService,
-    AngularFireAuth,
     ToastService,
     LoadingService
   ],
--- web/src/app/services/messaging.service.ts
+++ web/src/app/services/messaging.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
+// import { AngularFireDatabase } from 'angularfire2/database';
+// import { AngularFireAuth } from 'angularfire2/auth';
+// import * as firebase from 'firebase';
 
 
 import { BehaviorSubject } from 'rxjs'
@@ -11,12 +11,12 @@ import { take } from 'rxjs/operators';
 @Injectable()
 export class MessagingService {
 
-  messaging = firebase.messaging()
+  // messaging = firebase.messaging()
   currentMessage = new BehaviorSubject(null)
   user: any = {}
   constructor(
-    private db: AngularFireDatabase,
-    private afAuth: AngularFireAuth,
+    // private db: AngularFireDatabase,
+    // private afAuth: AngularFireAuth,
     private api: ApiService
   ) {
     this.user = JSON.parse(localStorage.getItem("user"))
@@ -30,42 +30,42 @@ export class MessagingService {
         isMobile: false
       }
     }).subscribe(() => {})
-    this.afAuth.authState.pipe(take(1)).subscribe(user => {
-      if (!user) return;
+    // this.afAuth.authState.pipe(take(1)).subscribe(user => {
+    //   if (!user) return;
 
-      const data = {
-        [user.uid]: token
-      }
-      this.db.object('fcmTokens/').update(data)
-      console.log("Token: ", token);
+    //   const data = {
+    //     [user.uid]: token
+    //   }
+    //   this.db.object('fcmTokens/').update(data)
+    //   console.log("Token: ", token);
 
-      // Add to localStorage
-      let tokenList : Array<any> = JSON.parse(localStorage.getItem("fireTokens"))
-      if(tokenList != null) { tokenList.push(token) }
-      else { tokenList = [ token ] }
-      localStorage.setItem("fireTokens", JSON.stringify(tokenList))
-    })
+    //   // Add to localStorage
+    //   let tokenList : Array<any> = JSON.parse(localStorage.getItem("fireTokens"))
+    //   if(tokenList != null) { tokenList.push(token) }
+    //   else { tokenList = [ token ] }
+    //   localStorage.setItem("fireTokens", JSON.stringify(tokenList))
+    // })
   }
 
   getPermission() {
-    this.messaging.requestPermission()
-      .then(() => {
-        console.log('Notification permission granted.');
-        return this.messaging.getToken()
-      })
-      .then(token => {
-        console.log(token)
-        this.updateToken(token)
-      })
-      .catch((err) => {
-        console.log('Unable to get permission to notify.', err);
-      });
+    // this.messaging.requestPermission()
+    //   .then(() => {
+    //     console.log('Notification permission granted.');
+    //     return this.messaging.getToken()
+    //   })
+    //   .then(token => {
+    //     console.log(token)
+    //     this.updateToken(token)
+    //   })
+    //   .catch((err) => {
+    //     console.log('Unable to get permission to notify.', err);
+    //   });
   }
 
   receiveMessage() {
-    this.messaging.onMessage((payload: any) => {
-      console.log("Message: ", payload);
-      this.currentMessage.next(payload)
-    });
+    // this.messaging.onMessage((payload: any) => {
+    //   console.log("Message: ", payload);
+    //   this.currentMessage.next(payload)
+    // });
   }
 }
