376ec49e5e371e61832e62dd9599c0109a63a372
German Cruz Moreno <cray.pro95@gmail.com>
2020-06-03 15:49:25 -0500

    push jalando

--- web/src/app/services/push.service.ts
+++ web/src/app/services/push.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 declare var OneSignal: any;
-
+const APP_ID="540412ae-b99e-4c4c-843f-f05dabbb6a25"
 @Injectable({
   providedIn: 'root'
 })
@@ -16,11 +16,7 @@ export class PushService {
 
   // Call this method to start the onesignal process.
   public init() {
-      // this.oneSignalInit ? console.log('Already Initialized') : this.addScript('https://cdn.onesignal.com/sdks/OneSignalSDK.js', (callback) => {
-      //     console.log('OneSignal Script Loaded');
-      //     this.initOneSignal();
-      //     this.oneSignalInit = true;
-      // })
+      this.oneSignalId = localStorage.getItem("pushtoken")
       this.initOneSignal();
   }
 
@@ -36,31 +32,42 @@ export class PushService {
   initOneSignal() {
     OneSignal.push(function() {
       OneSignal.init({
-        appId: "540412ae-b99e-4c4c-843f-f05dabbb6a25",
+        appId: APP_ID,
+        path:"/assets/OneSignal/",
+        // notifyButton: {
+        //   enable: true,
+        // },
+        persistNotification: false,
+        subdomainName: "templatejarabe",
       });
     });
     console.log('OneSignal Initialized');
-    OneSignal.push(["registerForPushNotifications"]);
-    // OneSignal.push(function () {
-    //     console.log('Register For Push');
-    //     OneSignal.setSubscription(true);
-    //     OneSignal.on('subscriptionChange', function (isSubscribed) {
-    //       console.log("The user's subscription state is now:", isSubscribed);
-    //           OneSignal.getUserId().then(function (userId) {
-    //               console.log("User ID is", userId);
-    //           });
-    //       });
+    // OneSignal.push(function() {
+    //   OneSignal.on('permissionPromptDisplay', function () {
+    //     console.log("The prompt displayed");
+    //     this.getUserID();
+    //   });
     // });
-    // this.checkIfSubscribed();
+   
+    if(this.oneSignalId){
+      console.log('Push notifications are enabled!');
+      OneSignal.push(function() {
+        OneSignal.on('notificationPermissionChange', function(permissionChange) {
+          var currentPermission = permissionChange.to;
+          console.log('New permission state:', currentPermission);
+        });        
+      });
+    }else{
+      this.checkIfSubscribed();
+    }
   }
 
   subscribe() {
     OneSignal.push(() => {
         console.log('Register For Push');
         OneSignal.push(['registerForPushNotifications'])
-        OneSignal.on('subscriptionChange', (isSubscribed) => {
+        OneSignal.on('subscriptionChange', function(isSubscribed){
             console.log('The user\'s subscription state is now:', isSubscribed);
-            this.listenForNotification();
             OneSignal.getUserId().then((userId) => {
                 console.log('User ID is', userId);
                 this.oneSignalId = userId;
@@ -74,7 +81,6 @@ export class PushService {
     console.log('Initalize Listener');
     OneSignal.on('notificationDisplay', (event) => {
         console.log('OneSignal notification displayed:', event);
-        this.listenForNotification();
     });
   }
 
@@ -86,22 +92,22 @@ export class PushService {
   }
 
   checkIfSubscribed() {
+    let self=this;
     console.log('checkIfSubscribed');
-    OneSignal.push(() => {
-        /* These examples are all valid */
-        OneSignal.isPushNotificationsEnabled((isEnabled) => {
-            if (isEnabled) {
-                console.log('Push notifications are enabled!');
-                this.getUserID();
-            } else {
-                console.log('Push notifications are not enabled yet.');
-                this.subscribe();
-            }
-        }, (error) => {
-            console.log('Push permission not granted');
-        });
-    });
-  }
+    OneSignal.push(function(){
+        OneSignal.isPushNotificationsEnabled().then(function(isEnabled){
+        console.log("enabled",isEnabled);
+        if (isEnabled) {
+          console.log('Push notifications are enabled!');
+          self.getUserID();
+          console.log('Push notifications are enabled!');
+        } else {
+          console.log('Push notifications are not enabled yet.');
+          self.subscribe();
+        }
+      });
+    })
+    }
 
   UpdatePushToken() {
     // let token = localStorage.getItem('token');  
