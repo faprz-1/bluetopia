1bd3b0e14bcbfd8dcd8183f399a19f97c5fda1d5
Ivan Pacheco <pacheco@jarabesoft.com>
2021-03-18 14:49:26 -0600

    Rework pagina de notficaciones

--- mobile/src/app/pages/notification/notification.page.ts
+++ mobile/src/app/pages/notification/notification.page.ts
@@ -1,13 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import * as moment from 'moment';
+import { NotificationsService } from 'src/app/services/notifications.service';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.page.html',
   styleUrls: ['./notification.page.scss'],
 })
-export class NotificationPage implements OnInit {
-
-  ngOnInit() {
+export class NotificationPage {
+  ionViewDidEnter() {
+    this.notifications.notifications = [];
+    this.notifications.LoadNotifications();
   }
 
+  DoRefresh(refresher) {
+    this.notifications.notifications = [];
+    this.notifications.LoadNotifications();
+    setTimeout(() => {
+      refresher.target.complete();
+    }, 500)
+  }
+
+  SelectNotification(notification) {
+    if(notification.data) {
+      if(notification.data.action) {
+        this.navController.navigateForward(
+          notification.data.action,
+          {
+            queryParams: notification.data.queryParams || {},
+          }
+        )
+      }
+    }
+  }
+
+  get currentNotifications() {
+    const notifications = JSON.parse(JSON.stringify(this.notifications.notifications));
+    notifications.forEach(notification => notification.timestamp = moment(notification.timestamp).fromNow());
+    return notifications;
+  }
+
+  constructor(
+    private notifications: NotificationsService,
+    private navController: NavController,
+  ) {}
+
+  NavigateBack() {
+    this.navController.back();
+  }
 }
