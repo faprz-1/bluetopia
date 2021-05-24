1ae86b7b0051eecf0b2355320baace1f76ee82d6
Ivan Pacheco <pacheco@jarabesoft.com>
2021-05-24 17:46:07 -0500

    Add Apple token to hidden user data

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   staging: false,
-  baseURL: "https://template.jarabeapi.com/api"
+  baseURL: "https://template.jarabeapi.com/api",
+  socketURL: "https://template.jarabeapi.com",
+  host: 'http://template.jarabepruebas.com/#',
 };
 
