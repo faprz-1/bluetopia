11f5e45fd90d4a747fa6e1630fc3bf956f11c4c6
adith <adithlbp@jarabesoft.com>
2021-05-26 16:09:28 -0500

    build tracker

---
+++ mobile/app-compile-staging.sh
@@ -0,0 +1,38 @@
+creator=$(hostname)
+angularConfiguration="staging"
+comand="ionic cordova build" 
+platform=$1
+logFile="builds-log.txt"
+
+if [ $platform = "android" ] 
+then  
+echo ""
+elif [ $platform = "ios" ]
+then
+echo ""
+else
+echo "
+----------------------------------
+Añade la plataforma, '$platform' no es android o ios
+----------------------------------" ; 
+exit 1; 
+fi
+
+echo "----------------------------------------------------------------------------------------------------
+[$creator] creando nueva version $platform ambiente $angularConfiguration $(date)
+----------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------
+[$creator] creando nueva version $platform ambiente $angularConfiguration $(date)
+----------------------------------------------------------------------------------------------------" > temp-$logFile
+cat $logFile >> temp-$logFile
+cat temp-$logFile > $logFile
+rm temp-$logFile
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "[$creator] Construyendo la aplicación... $comand $platform -c $angularConfiguration"
+echo "----------------------------------------------------------------------------------------------------"
+$comand $platform --prod -c $angularConfiguration
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "[$creator] Deployment completo."
+echo "----------------------------------------------------------------------------------------------------"
---
+++ web/web-compile-staging.sh
@@ -0,0 +1,24 @@
+creator=$(hostname)
+angularConfiguration="staging"
+comand="ng build" 
+logFile="builds-log.txt"
+
+
+echo "----------------------------------------------------------------------------------------------------
+[$creator] creando nueva version ambiente $angularConfiguration $(date)
+----------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------
+[$creator] creando nueva version ambiente $angularConfiguration $(date)
+----------------------------------------------------------------------------------------------------" > temp-$logFile
+cat $logFile >> temp-$logFile
+cat temp-$logFile > $logFile
+rm temp-$logFile
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "[$creator] Construyendo la aplicación... $comand -c $angularConfiguration"
+echo "----------------------------------------------------------------------------------------------------"
+$comand --prod -c $angularConfiguration
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "[$creator] Deployment completo."
+echo "----------------------------------------------------------------------------------------------------"
