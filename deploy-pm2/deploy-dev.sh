e5a7be1574eaead3f47646a5961b2777daf7ba91
German Cruz Moreno <>
2021-11-09 10:07:41 -0600

    pipelines

--- deploy-pm2/deploy-dev.sh
+++ deploy-pm2/deploy-dev.sh
@@ -1,16 +1,16 @@
 #bash
 # cd $ProjectFullPath/deploy-pm2
 echo "estoy en $PWD"
-# source config-dev.sh
+source config-dev.sh
 
-# echo "Liberando $project en $type"
-# cd $ProjectFullPath
-# echo "Trayendo ultimo commit"
+echo "Liberando $project en $type"
+cd $ProjectFullPath
+echo "Trayendo ultimo commit"
 git pull
-# echo "Copiando compiledAngular a nginx" # si es mas de un front... duplicar aqui.
-# rm -rf $Front_nginxFolder
-# sudo cp -R $sourceHtmlFolder $Front_nginxFolder
-# echo "Front Actualizado"
-# echo "Reiniciando PM2"
-# pm2 restart $project
+echo "Copiando compiledAngular a nginx" # si es mas de un front... duplicar aqui.
+rm -rf $Front_nginxFolder
+sudo cp -R $sourceHtmlFolder $Front_nginxFolder
+echo "Front Actualizado"
+echo "Reiniciando PM2"
+pm2 restart $project
 echo "Parece que ya se reinicio todo Felicidades"
