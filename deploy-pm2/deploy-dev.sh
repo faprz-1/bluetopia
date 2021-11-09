d8d9001a65c95c7b38f37527d2bc093639da9de2
German Cruz Moreno <>
2021-11-09 11:05:44 -0600

    pipelines

--- deploy-pm2/deploy-dev.sh
+++ deploy-pm2/deploy-dev.sh
@@ -14,9 +14,9 @@ echo "Liberando $project en $type"
 echo "Trayendo ultimo commit"
 git pull
 echo "Copiando compiledAngular a nginx $Front_nginxFolder" # si es mas de un front... duplicar aqui.
-echo "Copiando compiledAngular a nginx $sourceHtmlFolder $(ls $sourceHtmlFolder)" # si es mas de un front... duplicar aqui.
-# sudo rm -rf $Front_nginxFolder
-# sudo cp -R $sourceHtmlFolder $Front_nginxFolder
+echo "Copiando compiledAngular a nginx $sourceHtmlFolder $(sudo ls $sourceHtmlFolder)" # si es mas de un front... duplicar aqui.
+sudo rm -rf $Front_nginxFolder
+sudo cp -R $sourceHtmlFolder $Front_nginxFolder
 echo "Front Actualizado"
 echo "Reiniciando PM2"
 pm2 restart $project
