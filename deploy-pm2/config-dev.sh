af1d881bc1a2b137ec299f7b1abad326ce746a6a
German Cruz Moreno <>
2021-11-09 11:36:47 -0600

    pipelines

--- deploy-pm2/config-dev.sh
+++ deploy-pm2/config-dev.sh
@@ -2,7 +2,7 @@
 project="Template4.0"
 type="pm2 y nginx"
 repoFolderName="template4.0"
-repoPathName="~/repos/"
+repoPathName="/home/jarabe/repos/"
 Front_domain="template.jarabepruebas.com"
 Front_domainFolder="template_jarabe"
 Front_nginxFolder="/var/www/$Front_domainFolder"
