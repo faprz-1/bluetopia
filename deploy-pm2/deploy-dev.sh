8ff240f601f2973bcaeb0d6bd61525a016e99882
German Cruz Moreno <>
2021-11-10 09:39:37 -0600

    pipelines

--- deploy-pm2/deploy-dev.sh
+++ deploy-pm2/deploy-dev.sh
@@ -16,7 +16,7 @@ git pull
 echo "Eliminando $Front_nginxFolder" # si es mas de un front... duplicar aqui.
 rm -rf $Front_nginxFolder
 echo "Copiando compiledAngular a nginx" # si es mas de un front... duplicar aqui.
-mkdir Front_nginxFolder
+mkdir $Front_nginxFolder
 cp -R $sourceHtmlFolder $Front_nginxFolder
 echo "Front Actualizado"
 echo "Reiniciando PM2"
