5ea100dcc4165704acf1f31d884b5cebad69ded6
Antonio Romano <antonioromano@jarabesoft.com>
2022-11-07 10:18:33 -0600

    fix enviroment

--- web/src/environments/environment.staging.ts
+++ web/src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   staging: true,
   baseURL: "https://bluetopia.jarabeapi.com/api",
-  socketURL: "https://template.jarabeapi.com",
+  socketURL: "https://bluetopia.jarabeapi.com",
   host: 'http://bluetopia.jarabepruebas.com/#',
 };
   
