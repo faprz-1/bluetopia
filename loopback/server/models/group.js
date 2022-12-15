9819d9baf2be6ce897e2c3d26879217b983f90b2
Antonio Romano <antonioromano@jarabesoft.com>
2022-12-15 11:50:15 -0600

    adding models

--- loopback/server/models/group.js
+++ loopback/server/models/group.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Group) {
 
+    Group.CreateOne = function(group, callback) {
+        Group.create(group, (err, newGroup) => {
+            return callback(err, newGroup);
+        });
+    }
+
+    Group.GetAll = function(callback) {
+        Group.find({}, (err, groups) => {
+            return callback(null, groups);
+        });
+    }
+
 };
