102dc32e528a50493ddf4a5a364b028b86fbced2
Antonio Romano <antonioromano@jarabesoft.com>
2023-03-14 19:43:14 -0600

    adding slider components

---
+++ web/src/app/components/files-slider/files-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-files-slider',
+  templateUrl: './files-slider.component.html',
+  styleUrls: ['./files-slider.component.scss']
+})
+export class FilesSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ web/src/app/components/products-slider/products-slider.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductsSliderComponent } from './products-slider.component';
+
+describe('ProductsSliderComponent', () => {
+  let component: ProductsSliderComponent;
+  let fixture: ComponentFixture<ProductsSliderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductsSliderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductsSliderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
