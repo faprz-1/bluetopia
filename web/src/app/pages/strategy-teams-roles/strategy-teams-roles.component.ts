43946fa737152ae6ec8d34f9e8020d5188ce2b50
Antonio Romano <antonioromano@jarabesoft.com>
2023-08-28 10:22:38 -0600

    adding strategy teams components

---
+++ web/src/app/pages/strategy-teams-roles/strategy-teams-roles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-strategy-teams-roles',
+  templateUrl: './strategy-teams-roles.component.html',
+  styleUrls: ['./strategy-teams-roles.component.scss']
+})
+export class StrategyTeamsRolesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
