c49668287ef3b7dd9e2f1e75c018959a592cc8f4
Antonio Romano <antonioromano@jarabesoft.com>
2023-09-02 21:07:17 -0600

    adding relations

--- loopback/server/models/evaluation.js
+++ loopback/server/models/evaluation.js
@@ -2,4 +2,12 @@
 
 module.exports = function(Evaluation) {
 
+    Evaluation.Update = function(evaluation, callback) {
+        Evaluation.upsert(evaluation, (err, newEvaluation) => {
+            if(err) return callback(err);
+
+            return callback(null, newEvaluation);
+        });
+    }
+
 };
