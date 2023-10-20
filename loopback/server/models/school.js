8af0d12bd4e1e1e6da8324e1ad17819ea1a2c7f1
Aneth <anethballesteros@jarabesoft.com>
2023-10-20 09:33:41 -0600

    Fix edit school data. Still need fixes

--- loopback/server/models/school.js
+++ loopback/server/models/school.js
@@ -28,20 +28,18 @@ module.exports = function(School) {
 
     School.prototype.UpdateData = function(school, callback) {
         if(!school) return callback(null, false);
-        school.school.name = school.name
         School.upsert(school.school, (err, schoolupdated) => {
             if(err) return callback(err);
             School.app.models.UserData.Update(school.data, (err, updated) => {
         if(err) return callback(err);
-       let usuarioData = {
-        id : school.data.userId,
-            name : school.name,
-            email : school.email
-        }
-        School.app.models.Usuario.upsert(usuarioData, (err, updated) => {
+        School.app.models.Usuario.findOne({where: {id : school.data.userId}}, (err, user) => {
             if(err) return callback(err);
-
+           user.__data.username = school.name;
+           user.__data.email = school.email;
+        user.save(user, (err, updated) => {
+            if(err) return callback(err);
     return callback(null, school);
+});
 });
 });
 });
